0e757ccf57f9c6618cabace031bf017f0a09729b
lensilver <lensilverpr@hotmail.com>
2021-12-27 20:01:11 +0000

    users initial

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # корень сайта
   root "events#index"
   resources :events
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
