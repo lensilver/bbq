d83835cbe60aea28dc6c61add03b5bfbe884f848
lensilver <lensilverpr@hotmail.com>
2021-12-28 21:53:23 +0000

    Non-working version

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
 gem 'webpacker', '~> 5.0'
+gem 'twitter-bootstrap-rails'
 
 group :development, :test do  
   gem 'pry-rails'
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,10 @@
 class Event < ApplicationRecord
-	# Валидируем заголовок, он не может быть длиннее 255 букв
+  # Событие принадлежит юзеру
+  belongs_to :user
+  # Валидируем юзера на присутствие.
+  # валидируются по умолчанию
+  validates :user, presence: true
+  # Валидируем заголовок, он не может быть длиннее 255 букв
   validates :title, presence: true, length: {maximum: 255}
   # У события должны быть заполнены место и время
   validates :address, presence: true
