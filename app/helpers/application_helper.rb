37f6c969829ba5daa3cecb12e9a6e61e3ca88a57
lensilver <lensilverpr@hotmail.com>
2022-01-08 19:16:48 +0000

    Added bootstrap flash and font-awesome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  def bootstrap_class_for(name)
+    {
+     success: "alert-success",
+     error: "alert-danger",
+     danger: "alert-danger",
+     alert: "alert-warning",
+     notice: "alert-info"
+    }[name.to_sym] || name
+  end
+  
   def user_avatar(user)
     asset_pack_path('media/images/user.png')
   end
