7ec04199e5e4db07f0b85323b734b846f535e79a
lensilver <lensilverpr@hotmail.com>
2022-02-05 20:41:41 +0000

    change region for aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider:              'AWS',
-      region:                'eu-west-2',
+      region:                'eu-west-1',
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
