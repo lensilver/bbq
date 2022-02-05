ff5004c0d814c4438510c90ea8825ae975f25425
lensilver <lensilverpr@hotmail.com>
2022-02-05 07:49:48 +0000

    Disable subscription to your own event 8

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,5 +1,5 @@
 module SubscriptionsHelper
   def already_subscribed?(event)
-    current_user == @event.user || @event.subscribers.include?(current_user)
+    current_user == event.user || event.subscribers.include?(current_user)
   end
 end
