4a06f0abfa438a6949f2ff409177252f0d5ca015
lensilver <lensilverpr@hotmail.com>
2022-02-05 06:58:29 +0000

    Disable subscription to your own event 7

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,2 +1,5 @@
 module SubscriptionsHelper
+  def already_subscribed?(event)
+    current_user == @event.user || @event.subscribers.include?(current_user)
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -18,7 +18,7 @@ class Event < ApplicationRecord
   validates :address, presence: true
   validates :datetime, presence: true
 
-    def visitors
+  def visitors
     (subscribers + [user]).uniq
   end   
 end
