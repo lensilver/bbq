a2c07dd95cf6765b979a475769ad2b9c16ae6c3e
lensilver <lensilverpr@hotmail.com>
2022-02-18 16:35:09 +0000

    Added translation of the message subject 3

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,9 +2,6 @@ class Photo < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  # В 5-х Рельсах эти валидации не нужно явно прописывать
-  validates :event, presence: true
-  validates :user, presence: true
   validates :photo, presence: true, format: /[a-zA-Zа-яА-Я\-_0-9\/\:\.]*/i
 
   # Добавляем uploader, чтобы заработал carrierwave
