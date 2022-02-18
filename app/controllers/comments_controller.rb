9c9514cd1ec6b2e0096074fb86c4d2be8d29395f
lensilver <lensilverpr@hotmail.com>
2022-02-18 13:56:45 +0000

    Fixed mailing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
 
     if @new_comment.save
       # уведомляем всех подписчиков о новом комментарии
-      notify_subscribers(@event, @new_comment)
+      notify_subscribers(@new_comment)
 
       redirect_to @event, notice: I18n.t('controllers.comments.created')
     else
@@ -44,28 +44,18 @@ class CommentsController < ApplicationController
     params.require(:comment).permit(:body, :user_name)
   end
 
-  def notify_subscribers(event, comment)
+  def notify_subscribers(comment)
     # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    all_emails = (@event.subscriptions.map(&:user_email) + [@event.user.email]).uniq
 
     #Убираем из массива создателя коммента
-    all_emails.delete(comment.user.email) if comment.user.present?
+    all_emails.delete(comment.user.email)
 
     # По адресам из этого массива делаем рассылку
     # Как и в подписках, берём EventMailer и его метод comment с параметрами
     # И отсылаем в том же потоке
     all_emails.each do |mail|
-      EventMailer.comment(event, comment, mail).deliver_now
+      EventMailer.comment(comment, mail).deliver_now
     end
   end
 end
-
-def notify_subscribers(event, comment)
-      all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
-
-      all_emails.delete(comment.user.email) if comment.user.present?
-
-      all_emails.each do |mail|
-        EventMailer.comment(event, comment, mail).deliver_now
-      end
-    end
--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,36 +1,23 @@
 class EventMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.event_mailer.subscription.subject
-  #
-  def subscription(event, subscription)
+  def subscription(subscription)
     @email = subscription.user_email
     @name = subscription.user_name
-    @event = event
+    @event = subscription.event
 
-    # Берём у юзер его email
-    # Subject тоже можно переносить в локали
-    mail to: event.user.email, subject: "Новая подписка на #{event.title}"
+    mail to: @event.user.email, subject: default_i18n_subject(title: @event.title)
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.event_mailer.comment.subject
-  #
-  def comment(event, comment, email)
+  def comment(comment, email)
     @comment = comment
-    @event = event
+    @event = comment.event
 
-    mail to: email, subject: "Новый комментарий @ #{event.title}"
+    mail to: email, subject: default_i18n_subject(title: @event.title)
   end
 
-  def photo(event, photo, email)
+  def photo(photo, email)
     @photo = photo
-    @event = event
+    @event = photo.event
 
-    mail to: email, subject: "Новое фото @ #{event.title}"
+    mail to: email, subject: default_i18n_subject(title: @event.title)
   end
 end
