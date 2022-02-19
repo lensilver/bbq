9742e194e4f3d31211974450c991e79bb2e74cdd
lensilver <lensilverpr@hotmail.com>
2022-02-19 20:01:14 +0000

    Fixed comment mailing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -34,26 +34,17 @@ class CommentsController < ApplicationController
     @event = Event.find(params[:event_id])
   end
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_comment
     @comment = @event.comments.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def comment_params
     params.require(:comment).permit(:body, :user_name)
   end
 
   def notify_subscribers(comment)
-    # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
-    all_emails = (@event.subscriptions.map(&:user_email) + [@event.user.email]).uniq
+    all_emails = (@event.subscriptions.map(&:user_email) + [@event.user.email] - [comment.user&.email]).uniq
 
-    #Убираем из массива создателя коммента
-    all_emails.delete(comment.user.email)
-
-    # По адресам из этого массива делаем рассылку
-    # Как и в подписках, берём EventMailer и его метод comment с параметрами
-    # И отсылаем в том же потоке
     all_emails.each do |mail|
       EventMailer.comment(comment, mail).deliver_now
     end
